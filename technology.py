408d61bebbce8e9cc29f3742b673a28d056cfd32
RMorales25 <rj.m.m.2500@estudiantec.cr>
2025-02-16 18:52:45 -0600

    regular expression and path fix for technology parser

--- technology.py
+++ technology.py
@@ -1,10 +1,10 @@
-import os.path
+
 import re
-from re import split, match, findall, sub
+from re import split, match, findall
 import xml.etree.cElementTree as ET
 import xml.dom.minidom
 
-current_dir=os.path.dirname(__file__)
+from pathlib import Path
 
 class TechLibCell:
     '''
@@ -38,27 +38,26 @@ class Technology:
         object that references the root element of the Technology Library tree
     '''
 
-
     def __init__(self, tech):
         self.cells = []
 
-        with open(f"{current_dir}/templates/{tech}.v", 'r') as technology_file:
+        with open(f"{Path(__file__).parent}/templates/{tech}.v", 'r') as technology_file:
             content = technology_file.read()
 
             # split the technology file in modules
             modules = [f"module{l}module" for l in content.split('module')]
 
             for module in modules:
-                if (not 'input' in module or not 'output' or not 'endmodule' in module):
+                if (not 'input' in module or not 'output' in module):
                     continue
                 elif ('primitive' in module):
                     continue
                 else:
                     # extract the module information
-                    header = match(r'module (.+) \((?s:.+)\);', module)
+                    header = match(r'module (.+) \((.+)\);', module)
                     module_name = header.group(1)
-                    module_inputs = findall(r'input[\s\t]+(\w+)', module)
-                    module_outputs = findall(r'output[\s\t]+(\w+)', module)
+                    module_inputs = findall(r'input[\s\t]+(.+);', module)
+                    module_outputs = findall(r'output[\s\t]+(.+);', module)
                     # there was a fix to remove commas here
 
                     self.cells.append(
